3f42b00657d20b67a8b59afd30ca65c250c6ac7b
BrightTechnologies1 <silascyrax@gmail.com>
2022-09-26 02:18:42 +0100

    Responsiveness

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -15,22 +15,22 @@ function Body() {
   return (
     <div className="body">
           <div className='row col-12 body-content'>
-            <div className=' body-text col-6'>
+            <div className=' body-text col-lg-6 col-md-12 col-sm-12'>
                 <h1>About Us</h1>
                 <p>The First WEB3 Fashion Marketplace On The Solana Blockchain</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cupiditate accusantium, eos aliquid id sapiente officia amet nobis voluptatum ullam. Eum tempore illo labore ipsa, qui debitis accusantium. Odio, cupiditate?</p>
                 <Button variant="primary">Learn more</Button>{' '}
             </div>
-            <div className='body-video col-6'>
+            <div className='body-video col-lg-6 col-md-12 col-sm-12'>
                 <video src={asset2} autoPlay loop width={500} />
             </div>
           </div>
 
           {/* <div className='row col-12 body-content'>
-            <div className='body-video col-6'>
+            <div className='body-video col-lg-6 col-md-12 col-sm-12'>
                 <video src={asset2} autoPlay loop width={500} />
             </div>
-            <div className=' body-text col-6'>
+            <div className=' body-text col-lg-6 col-md-12 col-sm-12'>
                 <h1>About Us</h1>
                 <p>The First WEB3 Fashion Marketplace On The Solana Blockchain</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cupiditate accusantium, eos aliquid id sapiente officia amet nobis voluptatum ullam. Eum tempore illo labore ipsa, qui debitis accusantium. Odio, cupiditate?</p>
@@ -47,19 +47,19 @@ function Body() {
                 <p>The First WEB3 Fashion Marketplace On The Solana Blockchain</p>
               </div>
               <div className='utilities-body row col-12'>
-                <div className='utilities-item col-6'>
+                <div className='utilities-item col-lg-6 col-md-12 col-sm-12'>
                   <h2>Web3 Marketplace</h2>
                   <img src={marketplace} alt='' />
                 </div>
-                <div className='utilities-item col-6'>
+                <div className='utilities-item col-lg-6 col-md-12 col-sm-12'>
                   <h2>Web3 Marketplace</h2>
                   <img src={marketplace} alt='' />
                 </div>
-                <div className='utilities-item col-6'>
+                <div className='utilities-item col-lg-6 col-md-12 col-sm-12'>
                   <h2>Web3 Marketplace</h2>
                   <img src={marketplace} alt='' />
                 </div>
-                <div className='utilities-item col-6'>
+                <div className='utilities-item col-lg-6 col-md-12 col-sm-12'>
                   <h2>Web3 Marketplace</h2>
                   <img src={marketplace} alt='' />
                 </div>
@@ -72,35 +72,35 @@ function Body() {
                 <p>We are a fully Doxxed team of Developers, Artists, & Professionals who believe in giving back to the community that drives culture.</p>
               </div>
               <div className='team-body row col-12'>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
-                <div className='team-body-item col-3'>
+                <div className='team-body-item col-lg-3 col-md-6 col-sm-6'>
                   <img src={profile} alt='' />
                   <h2>Oxbryte</h2>
                 </div>
